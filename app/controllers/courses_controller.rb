05e88b2b25d0f6a9ce71238ed20b6ef6dc74965d
Margie Nevarez <margie.ynm@gmail.com>
2017-08-02 19:53:54 -0700

    create courses controller with index view

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,5 @@
+class CoursesController < ApplicationController
+  def index
+    @courses = []
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: "home#index"
+
+  get '/courses/search', to: 'courses#index'
 end
