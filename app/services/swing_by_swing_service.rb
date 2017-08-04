736017b1a61b72e1a33d2f9bd878232a3ce8ac0f
Margie Nevarez <margie.ynm@gmail.com>
2017-08-03 19:38:51 -0700

    extract course's hole data into model

--- app/services/swing_by_swing_service.rb
+++ app/services/swing_by_swing_service.rb
@@ -1,6 +1,6 @@
 class SwingBySwingService
   def self.search_courses(name)
-    url="https://api.swingbyswing.com/v2/courses/search_by_term?name=#{name}&active_only=yes&order_by=name&limit=20&access_token=#{ENV['access_token']}"
+    url = "https://api.swingbyswing.com/v2/courses/search_by_term?name=#{name}&active_only=yes&order_by=name&limit=20&access_token=#{ENV['access_token']}"
     response = RestClient.get(url).body
     rawCourseData = JSON.parse(response, symbolize_names: true)
 
@@ -25,5 +25,23 @@ class SwingBySwingService
       rawCourseDatum[:state_or_province]
     )
     course
+  end
+
+  def self.get_holes_for_course(course_id)
+    url = "https://api.swingbyswing.com/v2/courses/#{course_id}?access_token=#{ENV['access_token']}"
+    response = RestClient.get(url).body
+    rawCourseHolesData = JSON.parse(response, symbolize_names: true)[:course][:holes]
+
+    holes = rawCourseHolesData.map do |datum|
+      hole = Hole.new
+      hole.course_id = course_id
+      hole.hole_num = datum[:hole_num]
+      hole.par = datum[:tee_boxes][0][:par]
+      hole.yards = datum[:tee_boxes][0][:yards]
+
+      hole
+    end
+    
+    holes
   end
 end
