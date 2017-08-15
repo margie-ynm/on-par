0c913887ff7f924bd499c5fc2cf2e14f1a7ce5cc
Margie Nevarez <margie.ynm@gmail.com>
2017-08-15 12:33:30 -0700

    display scorecards on user's profile, edit routing for scorecard show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :scorecards, only: [:show]
+  end
 
   resources :courses, only: [:show] do
     get 'search', on: :collection, to: 'courses#index'
-    resources :scorecards, only: [:new, :create, :show] do
+    resources :scorecards, only: [:new, :create] do
       resources :hole_scores, only: [:new, :create]
     end
   end
