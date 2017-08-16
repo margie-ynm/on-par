4f2a930e65a6787a71d8b414c4e0e78d5d2599e2
Margie Nevarez <margie.ynm@gmail.com>
2017-08-15 17:54:57 -0700

    fix bug for detecting current date for scorecard creation

--- app/controllers/scorecards_controller.rb
+++ app/controllers/scorecards_controller.rb
@@ -7,16 +7,20 @@ class ScorecardsController < ApplicationController
 
   def new
     @scorecard = current_user.scorecards.new(course_id: params[:course_id])
-
   end
 
   def create
     course_id = params[:course_id]
-    @scorecard = current_user.scorecards.new(playdate: Time.new, course_id: course_id)
+    @scorecard = current_user.scorecards.new({course_id: course_id}.merge(scorecard_params))
     if @scorecard.save
       @scorecard.holes # using side effects to ensure dbase populated w/ course holes
       redirect_to new_course_scorecard_hole_score_path(course_id, @scorecard.id)
     end
+  end
+
+private
+  def scorecard_params
+    params.require(:scorecard).permit(:playdate)
   end
 
 end
