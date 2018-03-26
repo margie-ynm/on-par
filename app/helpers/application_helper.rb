f79937e80a8ea01e59227e75ccdf96534d5e9b58
Margie ynm <margie.ynm@gmail.com>
2018-03-26 16:07:51 -0700

    include sign in form partial for homepage, add devise methods in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
      notice: 'alert-info'
     }[flash_type.to_sym] || flash_type.to_s
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
