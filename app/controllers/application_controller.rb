cba53f07b3b85fcc4fbe01127530f488ac157917
Margie ynm <margie.ynm@gmail.com>
2018-03-26 18:59:22 -0700

    refactor root to redirect to login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource_or_scope)
+    puts "after_sign_in_path_for called"
+    if resource_or_scope.is_a?(User)
+      user_path(resource_or_scope)
+    else
+      root_path
+    end
+    # current_user
+  end
 end
